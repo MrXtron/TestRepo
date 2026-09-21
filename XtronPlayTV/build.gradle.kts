84611958017d1f49df653487795b872c04dc7e1e
MrXtron <275249936+MrXtron@users.noreply.github.com>
2026-09-21 23:11:46 +0530

    XtronPlayTV added

---
+++ XtronPlayTV/build.gradle.kts
@@ -0,0 +1,27 @@
+// use an integer for version numbers
+version = 13
+
+
+cloudstream {
+    language = "hi"
+    // All of these properties are optional, you can safely remove them
+    description = "Ultimate hub for Indian Daily Soaps & Dramas from BollyZone and DesiSerials"
+    authors = listOf("Xtron")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries"
+    )
+
+    iconUrl = "https://raw.githubusercontent.com/MrXtron/CloudStream-Extension/refs/heads/main/Files/Icons/XtronPlayTV.png"
+
+    isCrossPlatform = true
+}
---
+++ XtronPlayTV/src/main/kotlin/com/XtronPlayTV/XtronPlayTVPlugin.kt
@@ -0,0 +1,20 @@
+﻿package com.XtronPlayTV
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import android.content.Context
+
+@CloudstreamPlugin
+class XtronPlayTVPlugin: BasePlugin() {
+    override fun load() {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        val provider =  DesiSerialsProvider()
+        registerMainAPI(provider)
+        registerMainAPI(BollyzoneProvider())
+        registerExtractorAPI(Tvlogyflow((provider.name)))
+        registerExtractorAPI(Tellygossips((provider.name)))
+        registerExtractorAPI(Tvlogyflow((provider.name)))
+    }
+}
+
+
